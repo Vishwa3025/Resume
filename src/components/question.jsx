b928f869f6de4da0f7f85b903a04ceb04963880d
Vishwa R <89925780+Vishwa3025@users.noreply.github.com>
2023-09-25 14:25:06 +0530

    Update question.jsx

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -3,6 +3,9 @@ import Education from "./education";
 import PersonalDetails from "./personal_details";
 import Skills from "./skills";
 import Experience from "./experience";
+// import { Player } from "video-react";
+import v from "../assests/Dashboard.mp4";
+// import 'video-react/dist/video-react.css'; // import the video-react styles
 
 const Question = (props) => {
   return (
@@ -18,22 +21,27 @@ const Question = (props) => {
           CLEAR CV
         </button> */}
       </div>
-
       {/* Personal Details */}
-
       <PersonalDetails onChange={props.onChange} />
-
       {/* Skills */}
-
       <Skills />
-
       {/* Education */}
-
       <Education />
-
       {/* Experience */}
-
       <Experience />
+
+      {/* <Player
+        controlBar={{
+          playToggle: false, // Hide the play button
+          currentTimeDisplay: false, // Hide current time display
+          durationDisplay: false, // Hide duration time display
+          progressControl: false, // Hide the progress bar
+          fullscreenToggle: false, // Hide fullscreen button
+        }}
+        className="bg-blue-300">
+        <source src={v} />
+      </Player> */}
+      <video className="bg-blue-300" controls src={v} type="video/mp4"></video>
     </div>
   );
 };
